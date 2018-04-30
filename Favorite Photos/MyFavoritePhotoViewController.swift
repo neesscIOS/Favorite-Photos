798b62efcde3114f5767de5a9205f3bf7a5501f3
CSSE Department <csse@cssemm-06.wlan.rose-hulman.edu>
2018-04-30 11:58:07 -0400

    UI set up

---
+++ Favorite Photos/MyFavoritePhotoViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MyFavoritePhotoViewController.swift
+//  Favorite Photos
+//
+//  Created by CSSE Department on 4/30/18.
+//  Copyright © 2018 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class MyFavoritePhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBAction func takePhoto(_ sender: Any) {
+    }
+    
+    
+}
